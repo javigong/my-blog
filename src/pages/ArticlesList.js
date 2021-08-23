0d2953b0a88ceb3247c51f87a712409c85144cb0
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-08-23 15:44:21 -0700

    add excerpt to article list

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -6,8 +6,13 @@ const ArticlesList = () => (
   <>
     <h1>Articles</h1>
     {articleContent.map((article, key) => (
-      <Link to={`article/${article.name}`} key={key}>
+      <Link
+        className="article-list-item"
+        key={key}
+        to={`article/${article.name}`}
+      >
         <h3>{article.title}</h3>
+        <p>{article.content[0].substring(0, 150)}...</p>
       </Link>
     ))}
   </>
